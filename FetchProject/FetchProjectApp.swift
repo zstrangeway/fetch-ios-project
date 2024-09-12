32129a90d5e1af02b2ff54a800b6a3bd439d0d1d
zstrangeway <zac.strangeway@gmail.com>
2024-09-11 22:51:06 -0500

    Add new xcode project files

---
+++ FetchProject/FetchProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchProjectApp.swift
+//  FetchProject
+//
+//  Created by Zachary Strangeway on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
